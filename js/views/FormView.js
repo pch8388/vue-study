6c0ea46711d7744736f00521a16e6c1b278c1021
pch8388 <pch838811@gmail.com>
2020-08-06 23:27:22 +0900

    엔터 검색

--- js/views/FormView.js
+++ js/views/FormView.js
@@ -10,6 +10,7 @@ FormView.setup = function (el) {
     this.resetEl = el.querySelector('[type=reset]');
     this.showResetBtn(false);
     this.bindEvents();
+    return this;
 };
 
 FormView.showResetBtn = function (show = true) {
@@ -17,11 +18,15 @@ FormView.showResetBtn = function (show = true) {
 };
 
 FormView.bindEvents = function () {
+    this.on('submit', e => e.preventDefault());
     this.inputEl.addEventListener('keyup', e => this.onKeyup(e));
 };
 
-FormView.onKeyup = function() {
+FormView.onKeyup = function (e) {
     this.showResetBtn(this.inputEl.value.length);
+    if (e.code !== 'Enter') return;
+    this.emit('@submit', {input: this.inputEl.value});
+
 };
 
 export default FormView;
