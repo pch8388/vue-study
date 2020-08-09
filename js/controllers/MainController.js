2bdf2dcbbd457a7479130523f0e4b97f77dcd1bb
pch8388 <pch838811@gmail.com>
2020-08-09 23:10:57 +0900

    최근검색어 구현 1,2

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,6 +2,7 @@ import FormView from '../views/FormView.js';
 import ResultView from '../views/ResultView.js';
 import TabView from '../views/TabView.js';
 import KeywordView from "../views/KeywordView.js";
+import HistoryView from "../views/HistoryView.js";
 
 import SearchModel from "../models/SearchModel.js";
 import KeywordModel from "../models/KeywordModel.js";
@@ -22,9 +23,12 @@ export default {
         KeywordView.setup(document.querySelector('#search-keyword'))
             .on('@click', e => this.onClickKeyword(e.detail.keyword));
 
+        HistoryView.setup(document.querySelector('#search-history'))
+            .on('@click', e => this.onClickHistory(e.detail.keyword));
+
         ResultView.setup(document.querySelector('#search-result'));
 
-        this.selectedTab = '추천 검색어';
+        this.selectedTab = '최근 검색어';
         this.renderView();
 
     },
@@ -36,7 +40,7 @@ export default {
         if (this.selectedTab === '추천 검색어') {
             this.fetchSearchKeyword();
         } else {
-
+            this.fetchSearchHistory();
         }
 
         ResultView.hide();
@@ -45,6 +49,12 @@ export default {
     fetchSearchKeyword() {
         KeywordModel.list().then(data => {
             KeywordView.render(data);
+        });
+    },
+
+    fetchSearchHistory() {
+        HistoryModel.list().then(data => {
+            HistoryView.render(data);
         });
     },
 
@@ -85,6 +95,10 @@ export default {
     },
 
     onClickKeyword(keyword) {
+        this.search(keyword);
+    },
+
+    onClickHistory(keyword) {
         this.search(keyword);
     }
 }
