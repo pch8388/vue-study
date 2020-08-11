5ac7f6e878e69f0e32d38785450d635cd8d78e6a
pch8388 <pch838811@gmail.com>
2020-08-12 00:01:57 +0900

    vue - 검색결과 구현

--- vue/js/app.js
+++ vue/js/app.js
@@ -1,17 +1,32 @@
+import SearchModel from "./models/SearchModel.js";
+
 new Vue({
     el: '#app',
     data: {
-        query: ''
+        query: '',
+        submitted: false,
+        searchResult: []
     },
     methods: {
         onSubmit(e) {
-
+            this.search();
         },
         onKeyup() {
-            if (!this.query.length) this.onReset();
+            if (!this.query.length) this.resetForm();
         },
         onReset() {
+            this.resetForm();
+        },
+        search() {
+            SearchModel.list().then(data => {
+                this.submitted = true;
+                this.searchResult = data;
+            });
+        },
+        resetForm() {
             this.query = '';
+            this.submitted = false;
+            this.searchResult = [];
         }
     }
 });
