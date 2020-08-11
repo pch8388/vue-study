4766eef9e75048fa58329feb7ad5b82a0cbf3796
pch8388 <pch838811@gmail.com>
2020-08-12 00:52:35 +0900

    vue - 탭 구현

--- vue/js/app.js
+++ vue/js/app.js
@@ -5,7 +5,12 @@ new Vue({
     data: {
         query: '',
         submitted: false,
+        tabs: ['추천 검색어', '최근 검색어'],
+        selectedTab: '',
         searchResult: []
+    },
+    created() {
+        this.selectedTab = this.tabs[0];
     },
     methods: {
         onSubmit(e) {
@@ -16,6 +21,9 @@ new Vue({
         },
         onReset() {
             this.resetForm();
+        },
+        onClickTab(tabName) {
+            this.selectedTab = tabName;
         },
         search() {
             SearchModel.list().then(data => {
