972f19c4622e31ee26b7baff5aacb92c93c0d599
pch8388 <pch838811@gmail.com>
2020-08-13 00:08:00 +0900

    vue - 추천검색어 구현

--- vue/js/app.js
+++ vue/js/app.js
@@ -1,4 +1,5 @@
 import SearchModel from "./models/SearchModel.js";
+import KeywordModel from "./models/KeywordModel.js";
 
 new Vue({
     el: '#app',
@@ -7,10 +8,12 @@ new Vue({
         submitted: false,
         tabs: ['추천 검색어', '최근 검색어'],
         selectedTab: '',
+        keywords: [],
         searchResult: []
     },
     created() {
         this.selectedTab = this.tabs[0];
+        this.fetchKeyword();
     },
     methods: {
         onSubmit(e) {
@@ -22,8 +25,17 @@ new Vue({
         onReset() {
             this.resetForm();
         },
+        onClickKeyword(keyword) {
+            this.query = keyword;
+            this.search();
+        },
         onClickTab(tabName) {
             this.selectedTab = tabName;
+        },
+        fetchKeyword() {
+            KeywordModel.list().then(data => {
+                this.keywords = data;
+            });
         },
         search() {
             SearchModel.list().then(data => {
